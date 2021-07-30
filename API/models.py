be7000d1bd142e0545f6286b9ab93e09be71b924
MbuguaGeorge <mbuguag026@gmail.com>
2021-07-30 23:16:16 +0300

    add items in cart

--- API/models.py
+++ API/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+SHOES = 'shoes'
+CLOTHES = 'clothes'
+ACCESSORIES = 'accessories'
+
+CATEGORY = (
+    (SHOES,'shoes'),
+    (CLOTHES,'clothes'),
+    (ACCESSORIES,'accessories')
+)
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_category = models.CharField(choices=CATEGORY,max_length=100,default=SHOES)
+    product_price = models.FloatField()
+    thumbnail = models.ImageField(blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
